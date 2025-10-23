5fc3fa64d56c5f89efef2569b4758e3f82d8deeb
Krishna H K <khk803123@gmail.com>
2025-10-23 14:37:05 +0530

    Fix variable names for attendance calculation

---
+++ attendence.py
@@ -0,0 +1,8 @@
+
+c_held = int(input("Enter total number of classes held: "))
+c_attended = int(input("Enter number of classes attended: "))
+attendance = (c_attended / c_held) * 100
+
+print("Classes Held:", c_held)
+print("Classes Attended:", c_attended)
+print("Attendance:",attendance)
